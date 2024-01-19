b8de7044705b80c8f9cc6063dd4d6ce64936834f
gtrforworld <143692394+gtrforworld@users.noreply.github.com>
2024-01-20 02:32:09 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const nextConfig = {
         // nextScriptWorkers: true,
     },
     reactStrictMode: true,
-    webpack: config => {
+    webpack: (config, { dev, isServer }) => {
         config.resolve.fallback = { fs: false, net: false, tls: false };
         config.externals.push('pino-pretty', 'lokijs', 'encoding');
+        if (isServer) {
+            config.externals.push('mysql');
+        }
+    
         return config;
     },
 }
